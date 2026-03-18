77b096c6b9cacf6b4da9ca40e995f66266cd13dc
nbrfr55557-jpg <pierrick.stm@gmail.com>
2026-03-18 17:39:50 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'logbook-stan-v1';
+const FILES = [
+  '/LOGBOOK-STAN/logbook_muscu.html',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Mono:wght@400;500&family=DM+Sans:wght@300;400;500&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      // Mettre en cache les fichiers de polices Google Fonts
+      if(e.request.url.includes('fonts.googleapis.com') || e.request.url.includes('fonts.gstatic.com')) {
+        return caches.open(CACHE).then(c => {
+          c.put(e.request, res.clone());
+          return res;
+        });
+      }
+      return res;
+    }))
+  );
+});
